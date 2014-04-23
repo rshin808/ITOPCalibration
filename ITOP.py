17e64b0bd4ae4974f11a51f4932fcbca2332bb2c
root <root@beaglebone.(none)>
2014-04-23 21:11:51 +0000

    Adding ATT

--- ITOP.py
+++ ITOP.py
@@ -25,6 +25,13 @@ with open("EN", "rb") as ENFile:
     EN = bool(int(ENFile.read().split()[0]))
 
 # Helper Functions
+def drf(enable = False):
+    assert EN == True, "ERROR: Main Power Disabled"
+    RFOFF()
+   
+    if enable == True:
+        setRF(0, 1, 0)
+
 def ledsOff():
     for key in PINS.keys():
         if "LED" in str(key):
@@ -54,7 +61,7 @@ def setS(a = 1, b = 1):
     else:
         GPIO.output(PINS["SYS898_SELB"], GPIO.LOW)
 
-def setHATT(a = 1, b = 1, c = 1, d = 1, e = 1, f = 1):
+def setHBATT(a = 1, b = 1, c = 1, d = 1, e = 1, f = 1):
     if a == 1:
         GPIO.output(PINS["HB_RFATT_1"], GPIO.HIGH)
     else:
@@ -80,7 +87,7 @@ def setHATT(a = 1, b = 1, c = 1, d = 1, e = 1, f = 1):
     else:
         GPIO.output(PINS["HB_RFATT_6"], GPIO.LOW)
 
-def setLATT(a = 1, b = 1, c = 1, d = 1, e = 1, f = 1):
+def setLBATT(a = 1, b = 1, c = 1, d = 1, e = 1, f = 1):
     if a == 1:
         GPIO.output(PINS["LB_RFATT_1"], GPIO.HIGH)
     else:
@@ -107,11 +114,46 @@ def setLATT(a = 1, b = 1, c = 1, d = 1, e = 1, f = 1):
         GPIO.output(PINS["LB_RFATT_6"], GPIO.LOW)
 
 def setHATT(value = 0):
-    if float(int(value) * 2) == float(vlaue) * 2:
-        pass
-    
-    
+    l = "1"
+    if float(int(value) * 2) == float(value) * 2:
+        l = "1"
+    else:
+        l = "0"
 
+    b = ""
+    for i in range(5)[::-1]:
+        if(value - 2 ** i) >= 0:
+            value -= 2 ** i
+            b += "0"
+        else:
+            b += "1"
+    
+    b += 1
+    
+    setHBATT(int(b[0]), int(b[1]), int(b[2]), int(b[3]), int(b[4]), int(b[5]))  
+
+    return b
+
+def setLATT(value = 0):
+    l = "1"
+    if float(int(value) * 2) == float(value) * 2:
+        l = "1"
+    else:
+        l = "0"
+
+    b = ""
+    for i in range(5)[::-1]:
+        if(value - 2 ** i) >= 0:
+            value -= 2 ** i
+            b += "0"
+        else:
+            b += "1"
+    
+    b += l
+    
+    setLBATT(int(b[0]), int(b[1]), int(b[2]), int(b[3]), int(b[4]), int(b[5]))  
+    
+    return b
 
 def RFOFF():
     setRF()
@@ -123,6 +165,8 @@ def enable(parameters = None):
         Name:   enable
         Desc:   This enables the Main Power for the board.
                 The board is powered on with AUX enabled.
+        Params: parameters (list)
+                    None
     """
     for key in PINS.keys():
         GPIO.output(PINS[key], GPIO.LOW)
@@ -134,12 +178,18 @@ def enable(parameters = None):
 
     time.sleep(1)
 
-    aux(None)
+    aux([1])
 
     print "Main Power Enabled"
     print "Note: When finished use 'close' or 'shutdown'"
 
 def close(parameters = None):
+    """
+        Name:   close
+        Desc:   This closes the Main Power for the board.
+        Params: parameters (list)
+                    None
+    """
     for key in PINS.keys():
         if key != "MAINPW_EN":
             GPIO.output(PINS[key], GPIO.LOW)
@@ -154,102 +204,131 @@ def close(parameters = None):
     print "Main Power Disabled"
 
 def shutdown(parameters = None):
+    """
+        Name:   shutdown
+        Desc:   This shutdowns the board.
+        Params: parameters (list)
+                    None
+    """
     close()
     subprocess.call(["shutdown", "-h", "now"])
 
 def wgen(parameters = None):
+    """
+        Name:   wgen
+        Desc:   This enables WGEN.
+        Params: parameters (list)
+                    0:  enable (int)
+    """
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "WGEN only takes one parameter"
     RFOFF()
     
-    if parameters[0] == 1:
+    if int(parameters[0]) == 1:
         setRF(0, 0, 0)
-     
+        print "WGEN Enabled"
 
 def sine(parameters = None):
+    """
+        Name:   sine
+        Desc:   This enables SINE.
+        Params: parameters (list)
+                    0: enable (int)
+    """
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "SINE only takes one parameter"
     RFOFF()
 
-    if parameters[0] == 1:
+    if int(parameters[0]) == 1:
         setRF(1, 0, 0)
-
-def drf(parameters = None):
-    assert EN == True, "ERROR: Main Power Disabled"
-    assert len(parameters) == 1, "DRF only takes one parameter"
-    RFOFF()
-   
-    if parameters[0] == 1:
-        setRF(0, 1, 0)
-        master(1)
-        GPIO.output(PINS["MCLK_LED"], GPIO.HIGH)
-
-        print ""
-        print "DRF Enabled"
-        print "Note: MASTER is Enabled by default"
-        print "\tto change set either EXTT, LDT, or MASTER"
-        print "\tonly one may be set at a time"
-        print ""
+        print "SINE Enabled"
 
 def lbpm(parameters = None):
+    """
+        Name:   lbpm
+        Desc:   This enables LBPM.
+        Params: parameters (list)
+                    0: enable (int)
+    """
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "LBPM only takes one parameter"
     RFOFF()
 
-    if parameters[0] == 1:
+    if int(parameters[0]) == 1:
         setRF(1, 1, 0)
         GPIO.output(PINS["LBPM_LED"], GPIO.HIGH)
+        print "LBPM Enabled"
 
 def hbpm(parameters = None):
+    """
+        Name:   hbpm
+        Desc:   This enables HBPM.
+        Params: parameters (list)
+                0: enable (list)
+    """
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "HBPM only takes one parameter"
     RFOFF()
     
-    if parameters[0] == 1:
+    if int(parameters[0]) == 1:
         setRF(0, 0, 1)
         GPIO.output(PINS["HBPM_LED"], GPIO.HIGH)
+        print "HBPM Enabled"
 
 def aux(parameters = None):
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "AUX only takes one parameter"
     RFOFF()
     
-    if parameters[0] == 1:
+    if int(parameters[0]) == 1:
         setRF(1, 0, 1)
         GPIO.output(PINS["AUX_LED"], GPIO.HIGH)
+        print "AUX Enabled"
 
 def extt(parameters = None):
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "EXTT only takes one parameter"
     RFOFF()
+    
+    drf(1)
 
-    if parameters[0] == 1:
+    if int(parameters[0]) == 1:
         setS(0, 0) 
         GPIO.output(PINS["EXTT_LED"], GPIO.HIGH)
+        print "EXTT Enabled"
+
 
 def ldt(parameters = None):
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "LDT only takes one parameter"
     RFOFF()
     
-    if parameters[0] == 1:
+    drf(1)
+
+    if int(parameters[0]) == 1:
         setS(1, 0)
         GPIO.output(PINS["LDT_LED"], GPIO.HIGH)
+        print "LDT Enabled"
 
 def master(parameters = None):
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "MASTER only takes one parameter"
     RFOFF()
+
+    drf(1)
     
-    if parameters[0] == 1:
+    if int(parameters[0]) == 1:
         setS(1, 1)
         GPIO.output(PINS["MCLK_LED"], GPIO.HIGH)
+        print "MASTER Enabled"
 
 def lbatt(parameters = None):
     assert EN == True, "ERROR: Main Power Disabled"
     assert len(parameters) == 1, "LBATT only takes one parameter"
-    assert float(parameters[0]) <= 31.5 and float(paramters[0]) >= 0, "LBATT out of range (0 to 31.5)"
+    assert float(parameters[0]) <= 31.5 and float(parameters[0]) >= 0, "LBATT out of range (0 to 31.5)"
     assert float(parameters[0]) % 0.5 == 0, "LBATT resolution is 0.5 dB"
+    
+    print setLATT(float(parameters[0]))
 
 def rbatt(parameters = None):
     assert EN == True, "ERROR: Main Power Disabled"
@@ -258,7 +337,15 @@ def rbatt(parameters = None):
     assert float(parameters[0]) <= 31.5 and float(paramters[0]) >= 0, "RBATT out of range (0 to 31.5)"
     assert float(parameters[0]) % 0.5 == 0, "RBATT resolution is 0.5 dB"
 
+    print setHATT(float(parameters[0]))
+
 def helpMenu(parameters = None):
+    """
+        Name:   helpMenu
+        Desc:   This is the help menu for the ITOP Calibration module
+        Params: parameters (list):
+                    0:  fnumber (int), the function number
+    """
     if parameters == []:
         print ""
         print "HELP MENU"
@@ -271,54 +358,51 @@ def helpMenu(parameters = None):
         print "     1   |    CLOSE"
         print "     2   |    WGEN [enable]"
         print "     3   |    SINE [enable]"
-        print "     4   |    DRF [enable]"
-        print "     5   |    LBPM [enable]"
-        print "     6   |    HBPM [enable]"
-        print "     7   |    AUX [enable]"
-        print "     8   |    EXTT [enable]"
-        print "     9   |    LDT [enable]"
-        print "    10   |    MASTER [enable]"
-        print "    11   |    LBATT [value]"
-        print "    12   |    RBATT [value]"
-        print "    13   |    HELP [fnumber]"
+        print "     4   |    LBPM [enable]"
+        print "     5   |    HBPM [enable]"
+        print "     6   |    AUX [enable]"
+        print "     7   |    EXTT [enable]"
+        print "     8   |    LDT [enable]"
+        print "     9   |    MASTER [enable]"
+        print "    10   |    LBATT [value]"
+        print "    11   |    RBATT [value]"
+        print "    12   |    HELP [fnumber]"
         print ""
-    elif parameters == [0]:
-        pass
-    elif parameters == [1]:
-        pass
-    elif parameters == [2]:
-        pass
-    elif parameters == [3]:
-        pass
-    elif parameters == [4]:
-        pass
-    elif parameters == [5]:
-        pass
-    elif parameters == [6]:
-        pass
-    elif parameters == [7]:
-        pass
-    elif parameters == [8]:
-        pass
-    elif parameters == [9]:
-        pass
-    elif parameters == [10]:
-        pass
-    elif parameters == [11]:
-        pass
-    elif parameters == [12]:
-        pass
-    elif parameters == [13]:
-        pass
+    elif parameters == ["0"]:
+        print str(enable.__doc__)
+    elif parameters == ["1"]:
+        print str(close.__doc__)
+    elif parameters == ["2"]:
+        print str(wgen.__doc__)
+    elif parameters == ["3"]:
+        print str(sine.__doc__)
+    elif parameters == ["4"]:
+        print str(lbpm.__doc__)
+    elif parameters == ["5"]:
+        print str(hbpm.__doc__)
+    elif parameters == ["6"]:
+        print str(aux.__doc__)
+    elif parameters == ["7"]:
+        print str(extt.__doc__)
+    elif parameters == ["8"]:
+        print str(ldt.__doc__)
+    elif parameters == ["9"]:
+        print str(master.__doc__)
+    elif parameters == ["10"]:
+        print str(lbatt.__doc__)
+    elif parameters == ["11"]:
+        print str(rbatt.__doc__)
+    elif parameters == ["12"]:
+        print str(helpMenu.__doc__)
     else:
         print "Not a valid parameter"
+
 ITOP = {
     "ENABLE"    :   enable,
     "CLOSE"     :   close,
     "SHUTDOWN"  :   shutdown,     
     "WGEN"      :   wgen,
     "SINE"      :   sine,
-    "DRF"       :   drf,
     "LBPM"      :   lbpm,
     "HBPM"      :   hbpm,
     "AUX"       :   aux,
